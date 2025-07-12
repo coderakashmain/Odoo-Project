1b615d0ceec7fc7927fc227a9046fcc90b3a11c2
Akash Bindhani <ab791235@gmail.com>
2025-07-12 07:56:53 +0530

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const dotenv = require("dotenv");
+const cors = require("cors");
+const http = require("http");
+
+const cookieParser = require("cookie-parser");
+
+dotenv.config();
+const app = express();
+app.use(express.json({ limit: "50mb" }));
+const server = http.createServer(app);
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
